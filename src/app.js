12bd532896ac3b5916bd11a66b1be84da2dbc2f1
Unai <unaimartinezrodriguez@gmail.com>
2016-02-23 16:46:38 +0100

    Timer for pomodoro & getUp
    Works well on background, so I have to make the show() after finishing, plus, breaks

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 var UI = require('ui');
+var Vibe = require('ui/vibe');
 var waterAmount = 0;
+var pomodoroTime = 25;
+var getUpTime = 60;
+var smallBreak = 5;
+var longBreak = 15;
 
 var main = new UI.Menu({
   backgroundColor: 'black',
@@ -73,4 +78,46 @@ water.on('select', function(e) {
 function stringedWater(){
   var wa = waterAmount.toString();
   return (wa + ' cups of water today');
+}
+
+pomodoro.on('click', 'select', function(e) {
+  timer(pomodoroTime, pomodoro, 'Pomodoro');
+});
+
+getUp.on('click', 'select', function(e) {
+  timer(getUpTime, getUp, 'GetUp');
+});
+
+function finishedTimer(elementName, element) {
+  Vibe.vibrate('double');
+  switch(elementName) {
+    case 'Pomodoro':
+      pomodoroTime = 25;
+      element.title(pomodoroTime.toString() + ' minutes left!');
+      break;
+    case 'GetUp':
+      getUpTime = 60;
+      element.title(getUpTime.toString() + ' minutes left!');
+      break;
+    case 'Small':
+      smallBreak = 5;
+      element.title(smallBreak.toString() + ' minutes left!');
+      break;
+    case 'Long':
+      longBreak = 15;
+      element.title(longBreak.toString() + ' minutes left!');
+      break;
+  }
+}
+
+function timer(timeLeft, element, elementName) {
+  setTimeout(function (){
+    if (timeLeft > 0) {
+      timeLeft--;
+      element.title(timeLeft.toString() + ' minutes left!');
+      timer(timeLeft, element, elementName);
+    } else {
+      finishedTimer(elementName, element);
+    }
+  }, 60000);
 }
