0b5cf1bf11f79a2871d1d91cbe8e41b9bbb56a97
Unai <unaimartinezrodriguez@gmail.com>
2016-02-26 23:08:27 +0100

    Pomodoro time saved even closing the app

--- src/app.js
+++ src/app.js
@@ -22,6 +22,88 @@ function stringedWater(){
   return (wa + ' cups of water today');
 }
 
+function createWakeup(constantTime, elementName) {
+  Wakeup.schedule({
+    time: (Date.now() / 1000) + 60 * constantTime,
+    data: {
+      wElementName: elementName
+    }
+  },
+  function(e) {
+    if (e.failed) {
+      console.log('Wakeup failed: ' + JSON.stringify(e));
+    } else {
+      switch(elementName) {
+        case 'Pomodoro':
+          pomodoroWakeup = Wakeup.get(e.id);
+          localStorage.setItem("pomodoroWakeupId", e.id);
+          break;
+        case 'GetUp':
+          getUpWakeup = Wakeup.get(e.id);
+          localStorage.setItem("getUpWakeupId", e.id);
+          break;
+        case 'Small':
+          smallBreakWakeup = Wakeup.get(e.id);
+          localStorage.setItem("smallBreakWakeupId", e.id);
+          break;
+        case 'Long':
+          longBreakWakeup = Wakeup.get(e.id);
+          localStorage.setItem("longBreakWakeupId", e.id);
+          break;
+      }
+    }
+  }); 
+}
+
+function checkWakeup(elementName) {
+  switch(elementName) {
+    case 'Pomodoro':
+      if ((pomodoroWakeup === 0) && (localStorage.getItem("pomodoroWakeupId") !== null)) {
+        pomodoroWakeup = Wakeup.get(localStorage.getItem("pomodoroWakeupId"));
+      }
+      return pomodoroWakeup;
+    case 'GetUp':
+      if ((getUpWakeup === 0) && (localStorage.getItem("getUpWakeupId") !== null)) {
+        getUpWakeup = Wakeup.get(localStorage.getItem("getUpWakeupId"));
+      }
+      return getUpWakeup;
+    case 'Small':
+      if ((smallBreakWakeup === 0) && (localStorage.getItem("smallBreakWakeupId") !== null)) {
+        smallBreakWakeup = Wakeup.get(localStorage.getItem("smallBreakWakeupId"));
+      }
+      return smallBreakWakeup;
+    case 'Long':
+      if ((longBreakWakeup === 0) && (localStorage.getItem("longBreakWakeupId") !== null)) {
+        longBreakWakeup = Wakeup.get(localStorage.getItem("longBreakWakeupId"));
+      }
+      return longBreakWakeup;
+    default:
+      return false;
+  }
+}
+
+function wakeupSeconds(elementName) {
+  var wake = checkWakeup(elementName);
+  if ((wake !== false) || (wake !== 0)) {
+    return (Math.floor(wake.time - (Date.now() / 1000)));
+  } else {
+    return -1;
+  }
+}
+
+function timer(constantTime, element, elementName) {
+  if (checkWakeup(elementName) === 0) {
+    createWakeup(constantTime, elementName);
+  }
+  setTimeout(function (){
+    var secondsLeft = wakeupSeconds(elementName);
+    if (secondsLeft > 0) {
+      element.subtitle(Math.floor(secondsLeft / 60).toString() + ' minutes left');
+      timer(constantTime, element, elementName);
+    }
+  }, 1000);
+}
+
 // App
 var main = new UI.Menu({
   backgroundColor: 'black',
@@ -40,7 +122,6 @@ var main = new UI.Menu({
 });
 
 main.show();
-Wakeup.cancel('all');
 
 var pomodoro = new UI.Card({
   title: 'Pomodoro', 
@@ -103,90 +184,34 @@ pomodoro.on('click', 'select', function(e) {
   timer(POMODORO, pomodoro, 'Pomodoro');
 });
 
-function timer(constantTime, element, elementName) {
-  if (checkWakeup(elementName) === 0) {
-    createWakeup(constantTime, elementName);
+pomodoro.on('show', function(e) {
+  if (checkWakeup('Pomodoro')) {
+    timer(POMODORO, pomodoro, 'Pomodoro');
   }
-  setTimeout(function (){
-    var secondsLeft = wakeupSeconds(elementName);
-    if (secondsLeft > 0) {
-      element.subtitle(Math.floor(secondsLeft / 60).toString() + ' minutes left');
-      timer(constantTime, element, elementName);
-    }
-  }, 1000);
-}
-
-function createWakeup(constantTime, elementName) {
-  Wakeup.schedule({
-    time: (Date.now() / 1000) + 60 * constantTime,
-    data: {
-      wElementName: elementName
-    }
-  },
-  function(e) {
-    if (e.failed) {
-      console.log('Wakeup failed: ' + JSON.stringify(e));
-    } else {
-      switch(elementName) {
-        case 'Pomodoro':
-          pomodoroWakeup = Wakeup.get(e.id);
-          break;
-        case 'GetUp':
-          getUpWakeup = Wakeup.get(e.id);
-          break;
-        case 'Small':
-          smallBreakWakeup = Wakeup.get(e.id);
-          break;
-        case 'Long':
-          longBreakWakeup = Wakeup.get(e.id);
-          break;
-      }
-    }
-  }); 
-}
-
-function checkWakeup(elementName) {
-  switch(elementName) {
-    case 'Pomodoro':
-      return pomodoroWakeup;
-    case 'GetUp':
-      return getUpWakeup;
-    case 'Small':
-      return smallBreakWakeup;
-    case 'Long':
-      return longBreakWakeup;
-    default:
-      return false;
-  }
-}
-
-function wakeupSeconds(elementName) {
-  var wake = checkWakeup(elementName);
-  if (wake !== false) {
-    return (Math.floor(wake.time - (Date.now() / 1000)));
-  } else {
-    return -1;
-  }
-}
+});
 
 Wakeup.on('wakeup', function(e) {
   Vibe.vibrate('double');
   switch(e.data.wElementName) {
     case 'Pomodoro':
       pomodoroWakeup = 0;
+      localStorage.removeItem("pomodoroWakeupId");
       pomodoro.subtitle('Finished at: ' + Date(e.time));
       pomodoro.show();
       break;
     case 'GetUp':
       getUpWakeup = 0;
+      localStorage.removeItem("getUpWakeupId");
       getUp.subtitle('Finished at: ' + Date(e.time));
       getUp.show();
       break;
     case 'Small':
       smallBreakWakeup = 0;
+      localStorage.removeItem("smallBreakWakeupId");
       break;
     case 'Long':
       longBreakWakeup = 0;
+      localStorage.removeItem("longBreakWakeupId");
       break;
   }
 });
